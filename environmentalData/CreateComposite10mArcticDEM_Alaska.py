51de76c7052ac69eb0738bada3a764b22cade252
Timm Nawrocki <twnawrocki@alaska.edu>
2019-10-23 09:39:02 -0800

    Split Arctic DEM Processing
    Split the processing scripts for the Arctic DEM composite into an Alaska and Canada to create manageable file sizes.

---
+++ environmentalData/CreateComposite10mArcticDEM_Alaska.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------------
+# Create Composite 10 m Arctic DEM for Alaska
+# Author: Timm Nawrocki
+# Created on: 2019-10-19
+# Usage: Must be executed in an ArcGIS Pro Python 3.6 installation.
+# Description: "Create Composite 10 m Arctic DEM for Alaska" combines individual DEM tiles, reprojects to NAD 1983 Alaska Albers, and resamples to 10 m.
+# ---------------------------------------------------------------------------
+
+# Import packages
+import arcpy
+import os
+from beringianGeospatialProcessing import arcpy_geoprocessing
+from beringianGeospatialProcessing import create_composite_dem
+
+# Set root directory
+drive = 'D:/'
+root_directory = os.path.join(drive, 'ACCS_Work/Data/elevation/ArcticDEM_Alaska_10m')
+
+# Set arcpy working environment
+arcpy.env.workspace = os.path.join(drive, 'ACCS_Work/Projects/VegetationEcology/AKVEG_QuantitativeMap/Project_GIS/BeringiaVegetation.gdb')
+
+# Define input datasets
+tile_folder = os.path.join(root_directory, 'tiles')
+projected_folder = os.path.join(root_directory, 'tiles_projected')
+snap_raster = os.path.join(drive, 'ACCS_Work/Projects/VegetationEcology/AKVEG_QuantitativeMap/Project_GIS/Data_Input/areaOfInterest_Initial.tif')
+
+# Define output raster
+arctic10m_composite = os.path.join(root_directory, 'Alaska_ArcticDEM_Elevation_10m_AKALB_20191022.tif')
+
+# Define input and output arrays
+create_dem_inputs = [snap_raster]
+create_dem_outputs = [arctic10m_composite]
+
+# Create key word arguments
+create_dem_kwargs = {'tile_folder': tile_folder,
+                     'projected_folder': projected_folder,
+                     'cell_size': 10,
+                     'input_projection': 3413,
+                     'output_projection': 3338,
+                     'geographic_transformation': 'WGS_1984_(ITRF00)_To_NAD_1983',
+                     'input_array': create_dem_inputs,
+                     'output_array': create_dem_outputs
+                     }
+
+# Process the create polygon function with the point array
+arcpy_geoprocessing(create_composite_dem, **create_dem_kwargs)
