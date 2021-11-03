110dc2f3ed1a7a93c8782a66fb195fbeffe1fb39
Timm Nawrocki <twnawrocki@alaska.edu>
2021-11-02 18:56:44 -0800

    Update Sentinel-1 and -2 composites.
    Updated Sentinel-1 composites to include data from 2021. Updated Sentinel-2 composites to include data from 2021, use Surface Reflectance instead of Top-Of-Atmosphere Reflectance where possible, and apply cloud probability mask.

--- data_Reflectance/sentinel1/Sentinel1_vv_vh.js
+++ data_Reflectance/sentinel1/Sentinel1_vv_vh.js
@@ -1,14 +1,14 @@
 /* -*- coding: utf-8 -*-
 ---------------------------------------------------------------------------
-Median Growing-season Composite Sentinel 1 SAR for 2015-2020
+Median Growing-season Composite Sentinel 1 SAR for 2015-2021
 Author: Timm Nawrocki, Alaska Center for Conservation Science
-Last Updated: 2020-10-04
+Last Updated: 2021-10-21
 Usage: Must be executed from the Google Earth Engine code editor.
 Description: This script produces median composites using ascending orbitals for the VV and VH polarizations from Sentinel-1.
 ---------------------------------------------------------------------------*/
 
 // Define an area of interest geometry.
-var areaOfInterest = /* color: #0b4a8b */ee.Geometry.Polygon(
+var aoi = /* color: #0b4a8b */ee.Geometry.Polygon(
     [[[-136.94652621058978, 69.33001796032033],
         [-143.09886996058978, 70.3298935335601],
         [-150.08617464808978, 70.71089859691716],
@@ -28,25 +28,25 @@ var areaOfInterest = /* color: #0b4a8b */ee.Geometry.Polygon(
         [-135.1531950041738, 69.83531851993344]]]);
 
 // Import the Sentinel-1 Image Collection VV and VH polarizations within study area and date range
-var sentinel1 = ee.ImageCollection('COPERNICUS/S1_GRD')
-    .filterBounds(areaOfInterest)
-    .filter(ee.Filter.calendarRange(2015, 2020, 'year'))
+var s1 = ee.ImageCollection('COPERNICUS/S1_GRD')
+    .filterBounds(aoi)
+    .filter(ee.Filter.calendarRange(2015, 2021, 'year'))
     .filter(ee.Filter.calendarRange(6, 8, 'month'))
     .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'))
     .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH'))
     .filter(ee.Filter.eq('instrumentMode', 'IW'))
     .filter(ee.Filter.eq('orbitProperties_pass', 'ASCENDING'))
     .sort('system:time_start');
-print("Sentinel 1 (date-filtered:", sentinel1);
+print("Sentinel 1 (date-filtered:", s1);
 
 // Create a VV and VH composite from ascending orbits
-var vv = sentinel1.select('VV').median()
-var vh = sentinel1.select('VH').median()
+var vv = s1.select('VV').median()
+var vh = s1.select('VH').median()
 print(vv)
 print(vh)
 
 // Add image to the map.
-Map.centerObject(areaOfInterest);
+Map.centerObject(aoi);
 Map.addLayer(vv, {min: -30, max: 0}, 'vv');
 Map.addLayer(vh, {min: -30, max: 0}, 'vh');
 
@@ -54,16 +54,16 @@ Map.addLayer(vh, {min: -30, max: 0}, 'vh');
 Export.image.toDrive({
     image: vv,
     description: 'Sent1_vv',
-    folder: 'Beringia_Sentinel-1',
+    folder: 'Sent1',
     scale: 10,
-    region: areaOfInterest,
+    region: aoi,
     maxPixels: 1e12
 });
 Export.image.toDrive({
     image: vh,
     description: 'Sent1_vh',
-    folder: 'Beringia_Sentinel-1',
+    folder: 'Sent1',
     scale: 10,
-    region: areaOfInterest,
+    region: aoi,
     maxPixels: 1e12
 });
